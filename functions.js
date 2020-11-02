b6cc8f6f7d807c650f885ef14111232afbdb52da
Russell M <russellmacq@gmail.com>
2020-11-02 14:00:15 -0600

    Webpage is sort of working
    All navigation works correctly, CSS is completely unimplemented, stop() is almost completely unimplemented

--- functions.js
+++ functions.js
@@ -1,12 +1,9 @@
-var GlobalTime = 0;
 var noteTimes = [];
 var notes = [];
 var path = '';
 
-function start() {
-  GlobalTime = new Date();
-}
 function stop() {
+  spliceVideo(noteTimes, path)
 }
 function logNotes() {
   var date = new Date();
@@ -14,8 +11,16 @@ function logNotes() {
   var string = "<li>" + "On " + date.toString() + ": " + note + "</li>";
   document.getElementById('log-list').insertAdjacentHTML('afterbegin', string);
   document.getElementById('note-box').value = "";
-  notes.push(note)
-  noteTimes.push(date.toString())
+  notes.push(note);
+  noteTimes.push(["note", date.toString()]);
+  document.getElementById('buttons').innerHTML =  "<button type='button' id='end-button' onclick='endNote()'> End Note </button> <button type='button' id='note-button' onclick='logNotes()'> New Note Text </button>";
+}
+
+function endNote() {
+  var date = new Date();
+  noteTimes.push(["end", date.toString()]);
+  document.getElementById('buttons').innerHTML = "<button type='button' id='start-button' onclick='logNotes()'> Start Note </button>"
+  document.getElementById('log-list').insertAdjacentHTML('afterbegin', "Clip ended: " + date.toString());
 }
 
 function configure() {
