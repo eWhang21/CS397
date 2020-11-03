d999570149370c187da95f92d09ecf74ae75c627
Esther Whang <estherwhang@MacBook-Pro.local>
2020-11-03 06:07:38 -0800

    touched up on webpage, make sure to reverse direciton of added notes top to bottom

--- functions.js
+++ functions.js
@@ -1,26 +1,46 @@
-var noteTimes = [];
-var notes = [];
+var noteTimes_array = [];
+var notes_array = [];
 var path = '';
 
 function stop() {
-  spliceVideo(noteTimes, path)
+  spliceVideo(noteTimes_array, path)
 }
+
+function enterTitle() {
+  var notes_title = document.getElementById('title_input').value;
+  document.getElementById('Title').insertAdjacentHTML('afterbegin', notes_title);
+  document.getElementById('buttons').innerHTML =  "<button type='button' id='end-button' onclick='endNote()'> End Note </button> <button type='button' id='note-button' onclick='logNotes()'> New Note Text </button>";
+  document.getElementById('timestamps').insertAdjacentHTML('afterbegin',  "<li>" + ("START CLIP: ").bold() + date.toString()) + "<li>";
+}
+
+
+function startNotes() {
+  var date = new Date();
+  var note_input = document.getElementById('note-box').value;
+  document.getElementById('timestamps').insertAdjacentHTML('afterbegin', note_input);
+  document.getElementById('note-box').value = "";
+  notes_array.push(note_input);
+  noteTimes_array.push(["timestamps", date.toString()]);
+  document.getElementById('buttons').innerHTML =  "<button type='button' id='end-button' onclick='endNote()'> End Note </button> <button type='button' id='note-button' onclick='logNotes()'> New Note Text </button>";
+  document.getElementById('timestamps').insertAdjacentHTML('afterbegin',  "<li>" + ("START CLIP: ").bold() + date.toString()) + "<li>";
+
+}
+
 function logNotes() {
   var date = new Date();
-  var note = document.getElementById('note-box').value;
-  var string = "<li>" + "On " + date.toString() + ": " + note + "</li>";
-  document.getElementById('log-list').insertAdjacentHTML('afterbegin', string);
+  var note_input = document.getElementById('note-box').value;
+  document.getElementById('timestamps').insertAdjacentHTML('afterbegin', note_input);
   document.getElementById('note-box').value = "";
-  notes.push(note);
-  noteTimes.push(["note", date.toString()]);
-  document.getElementById('buttons').innerHTML =  "<button type='button' id='end-button' onclick='endNote()'> End Note </button> <button type='button' id='note-button' onclick='logNotes()'> New Note Text </button>";
+  notes_array.push(note_input);
+  noteTimes_array.push(["timestamps", date.toString()]);
+  document.getElementById('timestamps').insertAdjacentHTML('afterbegin', "<li>" + "On " + date.toString() + ": <br>" + note_input + "</li>");
 }
 
 function endNote() {
   var date = new Date();
-  noteTimes.push(["end", date.toString()]);
-  document.getElementById('buttons').innerHTML = "<button type='button' id='start-button' onclick='logNotes()'> Start Note </button>"
-  document.getElementById('log-list').insertAdjacentHTML('afterbegin', "Clip ended: " + date.toString());
+  noteTimes_array.push(["end", date.toString()]);
+  document.getElementById('buttons').innerHTML = "<button type='button' id='start-button' onclick='startNotes()'> Start Note </button>"
+  document.getElementById('timestamps').insertAdjacentHTML('afterbegin',  "<li>" + ("END CLIP: ").bold() + date.toString()) + "<li>";
 }
 
 function configure() {
